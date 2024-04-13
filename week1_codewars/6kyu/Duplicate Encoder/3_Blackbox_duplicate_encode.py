ed58df61ed58d0b7de94969e6f3ff2612e03e245
olkaleska <147612442+olkaleska@users.noreply.github.com>
2024-04-13 20:38:59 +0300

    Delete week1_codewars/6kyu directory

--- week1_codewars/6kyu/Duplicate Encoder/3_Blackbox_duplicate_encode.py
+++
@@ -1,9 +0,0 @@
-def duplicate_encode(word):
-    word = word.lower()
-    encoded = ""
-    for char in word:
-        if word.count(char) == 1:
-            encoded += "("
-        else:
-            encoded += ")"
-    return encoded
