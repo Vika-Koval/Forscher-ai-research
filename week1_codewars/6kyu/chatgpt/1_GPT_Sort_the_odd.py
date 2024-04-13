33c3146c7d5424b9836a5be5221139f5cd38aae3
olkaleska <147612442+olkaleska@users.noreply.github.com>
2024-04-13 20:53:53 +0300

    Додаєм звіт

--- week1_codewars/6kyu/chatgpt/1_GPT_Sort_the_odd.py
+++ week1_codewars/6kyu/chatgpt/1_GPT_Sort_the_odd.py
@@ -1,4 +1,4 @@
-# Усе проходить
+# https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/python
 def sort_array(source_array):
     odd_numbers = sorted((num for num in source_array if num % 2 != 0), reverse=False)
     
