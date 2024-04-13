aad1c1adb0d2860f2124799b5c16015b0e646213
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-13 20:41:22 +0300

    clarity fix

--- week1_codewars/7kyu/blackbox/solution.py
+++ week1_codewars/7kyu/blackbox/solution.py
@@ -1,45 +1,48 @@
 # link: https://www.codewars.com/kata/54ba84be607a92aa900000f1/train/python
 # Test 1 : using just the provided prompt
 
-# An isogram is a word that has no repeating letters, consecutive or non-
-# consecutive. Implement a function that determines whether a string that contains
-# only letters is an isogram. Assume the empty string is an isogram. Ignore letter 
-# case.
+# Request:
+    # An isogram is a word that has no repeating letters, consecutive or non-
+    # consecutive. Implement a function that determines whether a string that contains
+    # only letters is an isogram. Assume the empty string is an isogram. Ignore letter 
+    # case.
 
+# Blackbox's answer:
 def is_isogram(word):
     word = word.lower()
     return len(word) == len(set(word))
-
 # Test 1, Attempt 1, Result 50/50
 
 
 # Test 2 : using just the example
 
-# Write a function in python that can do the following:
-# isIsogram "Dermatoglyphics" = true
-# isIsogram "moose" = false
-# isIsogram "aba" = false
+# Request:
+    # Write a function in python that can do the following:
+    # isIsogram "Dermatoglyphics" = true
+    # isIsogram "moose" = false
+    # isIsogram "aba" = false
 
-
+# Blackbox's answer:
 def is_isogram(word):
     return len(word) == len(set(word.lower()))
-
 # Test 2, Attempt 1, Result 50/50
 
 
 # Test 3 : using the tests provided
 
-# Write a function that can pass the following tests:
-# def fixed_tests():
-#     @test.it('Basic Test Cases')
-#     def basic_test_cases():      
-#         test.assert_equals(is_isogram("Dermatoglyphics"), True )
-#         test.assert_equals(is_isogram("isogram"), True )
-#         test.assert_equals(is_isogram("aba"), False, "same chars may not be adjacent" )
-#         test.assert_equals(is_isogram("moOse"), False, "same chars may not be same case" )
-#         test.assert_equals(is_isogram("isIsogram"), False )
-#         test.assert_equals(is_isogram(""), True, "an empty string is a valid isogram" )
+# Request:
+    # Write a function that can pass the following tests:
+    # def fixed_tests():
+    #     @test.it('Basic Test Cases')
+    #     def basic_test_cases():      
+    #         test.assert_equals(is_isogram("Dermatoglyphics"), True )
+    #         test.assert_equals(is_isogram("isogram"), True )
+    #         test.assert_equals(is_isogram("aba"), False, "same chars may not be adjacent" )
+    #         test.assert_equals(is_isogram("moOse"), False, "same chars may not be same case" )
+    #         test.assert_equals(is_isogram("isIsogram"), False )
+    #         test.assert_equals(is_isogram(""), True, "an empty string is a valid isogram" )
 
+# Blackbox's answer:
 def is_isogram(word):
     """
     Returns True if the given word is an isogram, and False otherwise.
@@ -58,24 +61,25 @@ def is_isogram(word):
     
     # If we've made it through the entire word without returning False, return True
     return True
-
 # Test 3, Attempt 1, Result 50/50
 
 
 # Test 4 : a very brief formulation of the prompt
 
-# write a function in python that checks if the word is an isogram
+# Request:
+    # write a function in python that checks if the word is an isogram
 
+# Blackbox's answer:
 def is_isogram(word):
     return len(word) == len(set(word))
-
 # Test 4, Attempt 1, Result 45/50
 
-# you didn't consider that lowercase and uppercase letters are the same
+# Request2:
+    # you didn't consider that lowercase and uppercase letters are the same
 
+# Blackbox's answer:
 def is_isogram(word):
     return len(word) == len(set(word.lower()))
-
 # Test 4, Attempt 2, Result 50/50
 
 
