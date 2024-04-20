7e4381e7aaed10d908e9937ad2ca28671e4d6bd2
olkaleska <147612442+olkaleska@users.noreply.github.com>
2024-04-20 22:53:54 +0300

    Додаю працюючий код

--- week2_cms/puzzle/black_box.py
+++ week2_cms/puzzle/black_box.py
@@ -1,7 +1,5 @@
-# Цей код не має 1, але blackbox або просто надсилав мені мій ж код, або цей
-# хочу також сказати, що цей код має деякі цікаві рішення, як, наприклад, спроба використати &,
-# проте він не перевіряє чи на одному кольорі нема повторів чисел
-"""Puzzle check"""
+"""puzzle function"""
+
 import time
 from memory_profiler import memory_usage
 
@@ -21,23 +19,34 @@ def validate_board(board:list) -> bool:
     for j in range(5):
         elements = set()
         for i in range(5+j):
-            row = board[i][4-j:]
-            if len(row) < 3:
-                continue
-            if len(elements & set(row)) != 0:
-                return False
-            elements.update(row)
-
+            if board[i][4-j] != '*' and board[i][4-j] != ' ':
+                if board[i][4-j] in elements:
+                    return False
+                elements.add(board[i][4-j])
         for i in range(4+j):
-            col = [board[i][j] for i in range(9)]
-            if len(col) < 3:
-                continue
-            if len(elements & set(col)) != 0:
-                return False
-            elements.update(col)
-
+            if board[4+j][8-i] != '*' and board[4+j][8-i] != ' ':
+                if board[4+j][8-i] in elements:
+                    return False
+                elements.add(board[4+j][8-i])
+    # Для стовпців
+    for j in range(9):
+        elements = set()
+        for i in range(9):
+            if board[i][j] != '*' and board[i][j] != ' ':
+                if board[i][j] in elements:
+                    return False
+                elements.add(board[i][j])
+    # Для рядків
+    for i in range(9):
+        elements = set()
+        for j in range(9):
+            if board[i][j] != '*' and board[i][j] != ' ':
+                if board[i][j] in elements:
+                    return False
+                elements.add(board[i][j])
     return True
-if __name__=="__main__":
+
+if __name__ == "__main__":
     t0 = time.monotonic()
     # Викликаємо функцію для вимірювання пам'яті
     mem_usage = memory_usage((validate_board, (["**** ****", "***1 ****", "**  3****", "* 4 1****", "     9 5 ", " 6  83  *", "3   1  **", "  8  2***", "  2  ****"],)), interval=0.1)
@@ -45,10 +54,9 @@ if __name__=="__main__":
     dt = t1 - t0
     mem_used = max(mem_usage) - min(mem_usage)  # Різниця між максимальним та мінімальним значенням
     mem_usage_kb = [round(mem * 1024) for mem in mem_usage]
-    # print('У кілобайтах:',mem_usage_kb)
     total_memory_kb = sum(mem_usage_kb)
     print(f"Використано пам'яті: {total_memory_kb} KB")
     print("Час виконання:", dt)
 
-# Використано пам'яті: 256700 KB
-# Час виконання: 2.344000000040978
+# Використано пам'яті: 257120 KB
+# Час виконання: 2.0
