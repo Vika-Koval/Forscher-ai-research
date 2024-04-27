055c7cafdbb2a9458d052e42cd328bca686db2af
Sirska-Mariia <149333906+Sirska-Mariia@users.noreply.github.com>
2024-04-27 12:37:47 +0300

    added previous version of sieve_flavius

---
+++ week3_unittest/Sieve_Sort_songs_Happy/Sieve/gpt/previous_sieve.py
@@ -0,0 +1,17 @@
+def sieve_flavius(number:int)->int:
+    """
+    This function generates a list of lucky numbers, smaller than the given number.
+    """
+    numbers = list(range(1,number))
+    for num in numbers:
+        if num%2==0:
+            numbers.remove(num)
+    for numb in range(1,len(numbers)-1):
+        del_nums = []
+        try:
+            index = numbers[numb]
+        except IndexError:
+            break
+        for i in range(index-1, len(numbers), index):
+            del_nums.append(numbers[i])
+    return numbers
