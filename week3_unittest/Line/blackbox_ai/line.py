cebd94e0d4983bf4d6200d36c9d7c3c5ef132be6
Danylo <149315521+dantssm@users.noreply.github.com>
2024-04-27 16:05:15 +0300

    research update

--- week3_unittest/Line/blackbox_ai/line.py
+++ week3_unittest/Line/blackbox_ai/line.py
@@ -272,4 +272,74 @@ if __name__ == '__main__':
 # time after: 0.001s, coverage: 73%
 # blackbox improved the speed but the coverage decreased. also it removed too many tests to the point that almost all the tests passed on the old not fully working code
 #####################################################################################################
-# 
+# now let's test whether blackbox can identify which lines of code were not tested and what approach to use to test them
+# -----------------------------------------------------------------------------------------------------------------------------------
+    # Request1:
+    # can you now analyze which lines of my code were not covered by unittests?
+    # (unittests and code)
+# gave tutorilas how to install coverage
+# -----------------------------------------------------------------------------------------------------------------------------------
+    # Request2:
+    # can you tell which lines of code are not covered by tests just by looking at it?
+    # Blackbox's answer:
+        # The Point class's __init__ method does not have any tests that check if the a and b parameters are valid. If these parameters are expected to be numeric, you may want to add tests that check for invalid input.
+        # The Point class's __eq__ method is not tested with a non-Point object. It's possible that this method may not behave as expected when compared to a non-Point object.
+        # The Line class's __init__ method does not have any tests that check if the p1 and p2 parameters are valid Point objects. If these parameters are expected to be Point objects, you may want to add tests that check for invalid input.
+        # The Line class's intersect method has several conditional statements that are not tested. For example, the case where self.ax is None and other.ax is not None is not tested. Similarly, the case where self.b is None is not tested.
+# now the response is way better
+# i was actually surprised how good blackbox managed to identify where the code is not tested
+# of course it's not perfect but it's way better than how chatgpt performed on the same task
+#####################################################################################################
+# okay, now let's see if blackbox can review unittests for mistakes and give recommendations
+# i'll be using my old unittests i used in the past to pass my old code:
+from line import Point, Line
+import unittest
+class TestLine(unittest.TestCase):
+    def setUp(self):
+        self.line1 = Line(Point(-2, 5), Point(4, -1))
+        self.line2 = Line(Point(6, 5), Point(8, 7))
+        self.line3 = Line(Point(12, -19), Point(14, -17))
+        self.line4 = Line(Point(11, -8), Point(17, -14))
+        self.line5 = Line(Point(10, 0), Point(10, 8))
+        self.line6 = Line(Point(5, 0), Point(5, 8))
+        self.line7 = Line(Point(0, 5), Point(10, 5))
+    def test_intersect_basic(self):
+        intersection1 = self.line1.intersect(self.line2)
+        self.assertEqual(intersection1.x, 2)
+        self.assertEqual(intersection1.y, 1)
+        intersection2 = self.line1.intersect(self.line3)
+        self.assertEqual(intersection2.x, 17)
+        self.assertEqual(intersection2.y, -14)
+        intersection5 = self.line3.intersect(self.line4)
+        self.assertEqual(intersection5.x, 17)
+        self.assertEqual(intersection5.y, -14)
+        intersection9 = self.line5.intersect(self.line4)
+        self.assertEqual(intersection9.x, 10)
+        self.assertEqual(intersection9.y, -7)
+    def test_intersect_parrarel(self):
+        intersection3 = self.line2.intersect(self.line3)
+        self.assertEqual(intersection3, None)
+        intersection6 = self.line2.intersect(self.line5)
+        self.assertEqual(intersection6.x, 10)
+        self.assertEqual(intersection6.y, 9)
+        intersection7 = self.line5.intersect(self.line6)
+        self.assertEqual(intersection7, None)
+    def test_intersect_same(self):
+        intersection4 = self.line1.intersect(self.line4)
+        self.assertEqual(intersection4, -1)
+        intersection8 = self.line6.intersect(self.line6)
+        self.assertEqual(intersection8, 1)
+    def test_single_dot(self):
+        with self.assertRaises(ValueError):
+            Line(Point(3, 4), Point(3, 4))
+# -----------------------------------------------------------------------------------------------------
+        # Request1:
+        # okay, can you review the unittests i made myself and tell me if they are correct?
+        # (my unittests)
+# he didn't see anything wrong with them
+# alright but what about some very obvious mistakes? lets try it
+# -------------------------------------------------------------------------------------------------
+        # Request1:
+        # what about these tests? are they ok?
+        # (the tests gpt made but with wrong calculations here and there)
+# he noticed 1/3 mistakes
